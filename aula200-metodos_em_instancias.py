2823bc0de782129778a30ee0cd77ae7e13701ca8
Luiz <lom.raiol@gmail.com>
2024-07-03 15:38:18 -0300

    add aula 200

--- aula200-metodos_em_instancias.py
+++ aula200-metodos_em_instancias.py
@@ -1,4 +1,18 @@
-# Métodos em instâncias de classes python
+# AULA 200 - Métodos em instâncias de classes python
+# AULAS 200, 201
+# hard coded - É algo que foi escrito diretamente no código
 class Carro:
-    def __init__(self):
-        ...
+    def __init__(self,nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro(nome='Celta')
+print(celta.nome)
+celta.acelerar()
